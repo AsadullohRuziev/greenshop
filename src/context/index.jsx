19abfeb8b6a6ba28ded5c4e1eee3ac111d11ab31
Reactjs <asadullohruziev@gmail.com>
2022-02-07 11:28:43 +0500

    navbar finished

---
+++ src/context/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './style';
+
+const Root = () => {
+  return <Container>
+      <h1>Root ok</h1>
+  </Container>;
+};
+
+export default Root;
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './style';
+
+const Root = () => {
+  return <Container>
+      <h1>Root ppppppppp</h1>
+  </Container>;
+};
+
+export default Root;
